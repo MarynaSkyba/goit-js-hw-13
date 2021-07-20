ee16f9dc28771f1cef8679df772dbc41a272b941
Maryna <marynaskyba@gmail.com>
2021-07-20 17:20:32 +0300

    update hw13

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './css/styles.css';
+// import Notiflix from 'notiflix';
+// import fetchCards from './templates/fetchCards.hbs'
+// import SimpleLightbox from "simplelightbox"
+
+// Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
