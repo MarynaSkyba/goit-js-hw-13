0e6da7ed9186173a545c4be3a4707d18ee1200e1
Maryna <marynaskyba@gmail.com>
2021-07-25 17:45:12 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import './css/styles.css';
 import NewsApiService from './news-service'
 import Notiflix from 'notiflix';
 import cards from './templates/cards.hbs';
-import SimpleLightbox from "simplelightbox";
+// import SimpleLightbox from "simplelightbox";
 import axios from 'axios';
 
+// var lightbox = new SimpleLightbox('.gallery a');
 
 const refs = {
     searchForm: document.querySelector('.search-form'),
@@ -13,8 +14,6 @@ const refs = {
     loadMoreBtn: document.querySelector('.load-more'),
 }
 
-var lightbox = new SimpleLightbox('.gallery a');
-console.log(lightbox);
 
 const newsApiService = new NewsApiService();
 
@@ -24,38 +23,53 @@ refs.loadMoreBtn.addEventListener('click', onLoad)
 
 refs.loadMoreBtn.classList.add('is-hidden');
 
+
 function onSearch(e){
     e.preventDefault();
     newsApiService.query = e.currentTarget.elements.searchQuery.value;
     
    if (newsApiService.query.trim() === ''){
     Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-   } else {
+   }
+   else {
     refs.loadMoreBtn.disabled = true;
     refs.loadMoreBtn.classList.remove('is-hidden');
+
+    Notiflix.Notify.info(`"Hooray! We found ${newsApiService.fetchArticles().totalHits} images."`)
+    
     newsApiService.resetPage();
-    newsApiService.fetchArticles().then(hits => {
-        clearCardsCounteiner();
-        refs.loadMoreBtn.disabled = false;
-        appendCardsMarkup(hits);
-        // lightbox.refresh(hits);
-      
+    newsApiService.fetchArticles().then(hit => {
+        console.log(hit);
+    clearCardsCounteiner();
+    refs.loadMoreBtn.disabled = false;
+    appendCardsMarkup(hit);
+    lightbox.refresh(hit);    
    });}
 }
 
+
 function onLoad (){
     refs.loadMoreBtn.disabled = true;
-    newsApiService.fetchArticles().then(hits => {
-        appendCardsMarkup(hits);
-        refs.loadMoreBtn.disabled = false});
+    newsApiService.fetchArticles().then(hit => {
+        appendCardsMarkup(hit)
+        refs.loadMoreBtn.disabled = false}
+        );
+}
+
+
+function appendCardsMarkup(hit){
+ refs.galleryCards.insertAdjacentHTML('beforeend', cards(hit));
 };
 
-
-function appendCardsMarkup(hits){
-    refs.galleryCards.insertAdjacentHTML('beforeend', cards(hits));
-
-};
-
-function clearCardsCounteiner(){
+function clearCardsCounteiner () {
     refs.galleryCards.innerHTML = '';
 }
+
+// function hitStatus(hit) {
+//     if (hit.status === 400) {
+//         Notiflix.Notify.failure('"We are sorry, but you have reached the end of search results."');
+//        refs.loadMoreBtn.classList.add('is-hidden');
+
+//        console.log(hit.status);
+//     }
+// }
--- src/news-service.js
+++ src/news-service.js
@@ -6,26 +6,32 @@ export default class NewsApiService {
     constructor (){
         this.searchQuery = '';
         this.page = 1;
+        this.totalHits = '';
+
     }
 
     fetchArticles() {
         const url =`https://pixabay.com/api/?key=22578117-98ddcf36fbc3d0da8c48aeee6&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`;
-        
         return axios.get(url)
-     
-        .then(data => {    
+        .then(data => {  
             this.page +=1;
-            console.log(data.data.totalHits);
+        
             return data.data.hits;
-
         })
+        .catch(error =>console.log(error))
         ;  
     }
 
-   
+
+
     resetPage () {
         this.page = 1;
-    }
+    };
+
+    // getTotalHits () {
+    //     this.totalHits = data.data.totalHits;
+    //     console.log(totalHits)
+    // }
 
     
     get query() {
